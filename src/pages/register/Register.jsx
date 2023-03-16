34110b9dad06eb036d6b8c55712fc200f4d9ceb0
8490 <ethan@clarusway.com>
2023-03-16 11:37:04 +0300

    add Register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import underConst from "../../assets/const.png";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <img style={{ width: "80%", margin: "4rem 0" }} src={underConst} alt="" />
+    </div>
+  );
 };
 
 export default Register;
