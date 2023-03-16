b6952fccfc1557a363418320a2df6d8d7ed49f22
8490 <ethan@clarusway.com>
2023-03-16 11:09:18 +0300

    add detail page

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { useLocation, useParams } from "react-router-dom";
+import {
+  Description,
+  DetailContainer,
+  DetailImg,
+  DetailTitle,
+  InfoPart,
+} from "./Detail.style";
+import defaultImg from "../../assets/book.jpg";
 
 const Detail = () => {
   const { state } = useLocation();
@@ -7,7 +15,32 @@ const Detail = () => {
 
   console.log(state, id);
 
-  return <div>Detail</div>;
+  return (
+    <DetailContainer>
+      <DetailTitle>{state.volumeInfo.title}</DetailTitle>
+      <DetailImg>
+        <img
+          src={state.volumeInfo.imageLinks?.smallThumbnail || defaultImg}
+          alt="book-img"
+        />
+      </DetailImg>
+      <Description>{state.volumeInfo.description}</Description>
+      <InfoPart>
+        <p>
+          {state.volumeInfo.authors?.map((item) => (
+            <span key={item}>{item},</span>
+          ))}
+        </p>
+
+        <p>
+          {state.volumeInfo.publishDate}{" "}
+          {state.volumeInfo.publisher && (
+            <span>/ {state.volumeInfo.publisher}</span>
+          )}
+        </p>
+      </InfoPart>
+    </DetailContainer>
+  );
 };
 
 export default Detail;
